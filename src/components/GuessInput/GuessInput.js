ddbcbdc5d42754300bd72157ba72c03c5f747247
Simon Pratt <simon@pr4tt.com>
2023-12-10 15:55:31 -0600

    ex1 use pattern

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -1,5 +1,22 @@
 import React from 'react';
 
+/*
+
+  Acceptance Criteria:
+
+    [X] Create a new component.
+        [X] Don't forget, you can use an NPM script to generate the scaffolding for you! We learn how to do this in the “Getting Started” video
+    [X] This component should render a <form> tag, including a label and a text input.
+    [X] The text input should be controlled by React state.
+    When the form is submitted:
+        [X] The entered value should be logged to the console (for now).
+        [X] The input should be reset to an empty string.
+    [X] The user's input should be converted to ALL UPPERCASE. No lower-case letters allowed.
+    [X] The input should have a minimum and maximum length of 5.
+        [X] NOTE: The minLength validator is a bit funky; you may wish to use the pattern attribute instead. This is discussed in more detail on the Solution page.
+
+ */
+
 function GuessInput({ makeGuess }) {
     const [guess, setGuess] = React.useState('');
 
@@ -19,7 +36,7 @@ function GuessInput({ makeGuess }) {
     
     return (<form class="guess-input-wrapper" onSubmit={submitGuess}>
                <label for="guess-input">Enter guess:</label>
-                <input id="guess-input" type="text" value={ guess } onChange={() => changeGuess(event.target.value)} />
+                <input id="guess-input" type="text" pattern="\w{5}" value={ guess } onChange={() => changeGuess(event.target.value)} />
             </form>);
 }
 
